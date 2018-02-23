7309c4c1a3d434b934d1bb3e95eb5fb668eded51
William Rotchford <will@Williams-MacBook-Pro.local>
2018-02-23 00:31:21 -0600

    - Added MYSQL functionality / Updated user guide

--- scraper/scraperTest.js
+++ scraper/scraperTest.js
@@ -2,9 +2,10 @@
  * Requires Node JS and npm library 'table-scraper'
  * Using Node JS version 8.9.4
  * npm install table-scraper
-
+ * npm install mysql
  */
 const scraper = require('table-scraper');
+const mysql = require('mysql');
 
 
 // Default error
@@ -14,26 +15,43 @@ scraper
         .get('http://www.x-rates.com/table/?from=USD&amount=1')
         .then(function (tableData) {
             var table = tableData[1];
-            var countryList = [];
-            var exchangeRate = [];
-
+    
+            // Establish SQL Connection to local DB
+            // Ensure these values are correct prior to running
+            const con = mysql.createConnection({
+                host: 'localhost',
+                user: 'admin',
+                password: 'admin',
+                database: 'currencydb'
+            });
+            
+            // Make sure table exists and is empty to prevent redundant entries
+            con.query('CREATE TABLE IF NOT EXISTS curRates (country VARCHAR(30), rate FLOAT)');
+            con.query('TRUNCATE TABLE curRates');
+            
             // Iterate through the table and retrieve all values 
             for (var i = 0; i < table.length; ++i) {
-
-                countryList.push(table[i]["US Dollar"]);
-                exchangeRate.push(parseFloat(table[i]["1.00 USD"]));
+                // Push Currency and exchange rates to table
+                var result = {country: table[i]["US Dollar"], rate: parseFloat(table[i]["1.00 USD"])};
+                con.query('INSERT INTO curRates SET ?', result, err => {
+                    if (err)
+                        throw err;
+                });
             }
-            // Verify that all the information has been scraped successfully
-                console.log('Data received from Db:\n');
-
-                console.log(countryList);
-                console.log(exchangeRate);
+            con.query('SELECT * FROM curRates', (err, rows) => {
+                if (err)
+                    throw err;
                 
-             /* 
-             * TO DO:
-             *          ADD SOME VARIABLE ERROR CHECKING
-             *          CURRENTLY OUTPUTS BLANKET ERROR STATEMENT
-             */
+                console.log('Data received from Db:\n');
+                console.log(rows);
+                // Ensure Process doesn't just hang
+                process.exit(-1);
+            });
+                
+
         }, function () {
             console.log(err);
         });
+/* 
+*          Check into creating EXE
+*/
